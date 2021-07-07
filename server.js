6c942065d472de6df4d212a6a475b5c527fc05ea
minuet1215 <minuet1215>
2021-07-07 15:54:07 +0900

    Create Customer Database Table & Node.js Database Connection Settings

--- server.js
+++ server.js
@@ -1,46 +1,30 @@
+const fs = require("fs");
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const data = fs.readFileSync("./database.json");
+const conf = JSON.parse(data);
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database,
+});
+
+connection.connect();
 
 app.get("/api/customers", (req, res) => {
-  res.send([
-    {
-      id: 1,
-      image: "https://placeimg.com/64/64/1",
-      name: "Min-Woo",
-      birthday: "951215",
-      gender: "Male",
-      job: "students",
-    },
-    {
-      id: 2,
-      image: "https://placeimg.com/64/64/2",
-      name: "Ji-Won",
-      birthday: "970101",
-      gender: "Female",
-      job: "students",
-    },
-    {
-      id: 3,
-      image: "https://placeimg.com/64/64/3",
-      name: "Yu-Min",
-      birthday: "981234",
-      gender: "Female",
-      job: "office worker",
-    },
-    {
-      id: 4,
-      image: "https://placeimg.com/64/64/4",
-      name: "Kyeong-Hoon",
-      birthday: "961234",
-      gender: "Male",
-      job: "students",
-    },
-  ]);
+  connection.query("SELECT * FROM CUSTOMER", (err, rows, fields) => {
+    res.send(rows);
+  });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
