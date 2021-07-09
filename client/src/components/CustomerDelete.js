6c2779081df4220a9a88d562c98c985ca66ca082
minuet1215 <minuet1215>
2021-07-09 15:41:24 +0900

    Add Material UI Design for Add/Delete

--- client/src/components/CustomerDelete.js
+++ client/src/components/CustomerDelete.js
@@ -1,6 +1,31 @@
 import React, { Component } from "react";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogContent from "@material-ui/core/DialogContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 
 class CustomerDelete extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+    };
+  }
+
+  handleClickOpen = () => {
+    this.setState({
+      open: true,
+    });
+  };
+
+  handleClose = () => {
+    this.setState({
+      open: false,
+    });
+  };
+
   deleteCustomer(id) {
     // /api/customers/7 : 고객 데이터가 7인 것을 삭제
     const url = "/api/customers/" + id;
@@ -12,13 +37,40 @@ class CustomerDelete extends Component {
 
   render() {
     return (
-      <button
-        onClick={(e) => {
-          this.deleteCustomer(this.props.id);
-        }}
-      >
-        Delete
-      </button>
+      <div>
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={this.handleClickOpen}
+        >
+          삭제
+        </Button>
+
+        <Dialog open={this.state.open} onClose={this.handleClose}>
+          <DialogTitle onClose={this.handleClose}>삭제경고</DialogTitle>
+          <DialogContent>
+            <Typography gutterBottom>선택한 고객 정보가 삭제됩니다.</Typography>
+          </DialogContent>
+          <DialogActions>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={(e) => {
+                this.deleteCustomer(this.props.id);
+              }}
+            >
+              Delete
+            </Button>
+            <Button
+              variant="outlined"
+              color="primary"
+              onClick={this.handleClose}
+            >
+              Cancel
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
     );
   }
 }
